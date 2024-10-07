89663e6193d8c955238293b936e0cc1cec1da74a
Baris Aydek <baris.aydek@gmail.com>
2024-10-07 22:17:42 +0300

    ilk sayfa

--- convert-fonts.js
+++ convert-fonts.js
@@ -1,36 +1,20 @@
-// TODO: WIP
 const fs = require("fs").promises;
 const path = require('path');
-const crypto = require('crypto');
-
-const inputFilePath = path.join(__dirname, 'tsbbew.html');
-
-const fontsDirPath = path.join(__dirname, 'fonts');
 
 (async () => {
   try {
-    await fs.mkdir(fontsDirPath, { recursive: true });
-
-    let htmlContent = await fs.readFile(inputFilePath, 'utf8');
+    // Read the base64 content from the file
+    let fontContent = await fs.readFile("fonts/ff1.woff", 'utf8');
     
-    const fonts = [];
-
-    let match;
-
-    const regex = /src:url\('data:application\/font-woff;base64,(.*?)'\)format\("woff"\);/g;
-
-    while ((match = regex.exec(htmlContent)) !== null) {
-      const md5sum = crypto.createHash('md5');
-      const fileName = `${md5sum.update(match[1]).digest('hex')}.woff`;
-      const filePath = path.join(fontsDirPath, fileName);
-
-      htmlContent = htmlContent.replace(match[1], fileName);
-
-      await fs.writeFile(filePath, match[1], 'base64');
-    }
-
-    await fs.writeFile(inputFilePath + ".new", htmlContent, 'utf8');
+    // Convert base64 to binary buffer
+    let binaryBuffer = Buffer.from(fontContent, 'base64');
+    
+    // Write the binary buffer to a new file
+    const outputPath = path.join('fonts', 'ff1_converted.woff');
+    await fs.writeFile(outputPath, binaryBuffer);
+    
+    console.log(`Conversion complete. Binary font file saved to: ${outputPath}`);
   } catch (error) {
-    console.error(error);
+    console.error('An error occurred:', error);
   }
 })();
